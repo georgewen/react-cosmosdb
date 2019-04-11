f9b31c026bc18735d3e62ec6b8acbda95871c007
Wen, George <George.Wen@smec.com>
2019-04-11 12:08:29 +1000

    working copy

---
+++ server/env/environment.js
@@ -0,0 +1,9 @@
+const cosmosPort = 27017; // replace with your port
+const dbName = 'admin';
+const key = '--';
+
+module.exports = {
+  cosmosPort,
+  dbName,
+  key
+};
--- server/mongo.js
+++ server/mongo.js
@@ -3,11 +3,21 @@ const env = require('./env/environment');
 
 mongoose.Promise = global.Promise;
 
-const mongoUri = `mongodb://${env.dbName}.documents.azure.com:${env.cosmosPort}/?ssl/=true`;
+//const mongoUri = `mongodb://${env.dbName}.documents.azure.com:${env.cosmosPort}/?ssl/=true`;
+const mongoUri = `mongodb://localhost:27017/admin`
 
+//function connect() {
+  //return mongoose.connect(mongoUri, { auth: { user: env.dbName, password: env.key }});
+//}
 function connect() {
-  return mongoose.connect(mongoUri, { auth: { user: env.dbName, password: env.key }});
-}
+  mongoose.set('debug', true);
+    return mongoose.connect(
+      mongoUri,
+      {
+        useMongoClient: true
+      }
+    );
+  }
 
 module.exports = {
   connect,
